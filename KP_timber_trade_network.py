7eaceabea5545c7a0decb6873a13b04ea0e29c42
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2022-06-07 22:34:51 -0400

    creates networks, computes indices, and creates figures

--- KP_timber_trade_network.py
+++ KP_timber_trade_network.py
@@ -317,3 +317,172 @@ df_wood_pulp.to_csv(direc + 'data/wood_pulp.csv', index = False)
 df_industrial_roundwood_non_coniferous_tropical.to_csv(direc + 'data/industrial_roundwood_non_coniferous_tropical.csv', index = False)
 df_wood_chips_and_particles.to_csv(direc + 'data/wood_chips_and_particles.csv', index = False)
 
+# Finally, creating some trade network figures for the paper using the df_forest_products dataframe
+
+# Recreate the trade network
+
+subdata = data[data.Item == 'Forest products (export/import)']
+subdata = subdata[subdata.Element == 'Export Value']
+
+M = np.zeros((len(nations),len(nations)))
+
+for i in range(len(nations)):
+    
+    for j in range(len(nations)):
+        
+        tmp = subdata[subdata['Reporter Countries'] == nations[i]].reset_index(drop = True)
+        tmp = tmp[tmp['Partner Countries'] == nations[j]].reset_index(drop = True)
+        
+        # Extract values
+        
+        if len(tmp) > 0:
+            
+            M[i][j] = tmp[nv][0]
+            
+N = nx.DiGraph(M)
+
+# Compute eigenvector and betweenness centralities and store as network attributes
+
+bc = nx.betweenness_centrality(N, weight = 'weight')
+ec = nx.eigenvector_centrality_numpy(N, weight = 'weight')
+nx.set_node_attributes(N, bc, 'bc')
+nx.set_node_attributes(N, ec, 'ec')
+
+# Computing total exports and store as network attributes
+
+te = dict(zip(N.nodes, sum(M)))
+mute = np.mean(sum(M))
+nx.set_node_attributes(N, te, 'te')
+
+# Define node colors and sizes
+
+node_color_bc = [float(N.nodes[v]['bc']) for v in N]
+node_color_ec = [float(N.nodes[v]['ec']) for v in N]
+node_size = [10*float(N.nodes[v]['te']) / mute for v in N]
+
+# Setting node labels
+
+nations[15] = 'BEL'
+nations[29] = 'CAN'
+nations[33] = 'CHI'
+nations[57] = 'FRA'
+nations[62] = 'GER'
+nations[79] = 'ITA'
+nations[81] = 'JAP'
+nations[101] = 'MEX'
+nations[107] = 'NED'
+nations[125] = 'KOR'
+nations[143] = 'SPA'
+nations[162] = 'UK'
+nations[164] = 'USA'
+mapping = dict(zip(N, nations))
+N = nx.relabel_nodes(N, mapping)
+
+# Drawing
+
+def drawer(cent):
+    
+    if cent < 5.68152066e-03:
+        
+        cent = 5.68152066e-03
+        
+    theta = 2 * np.pi * np.random.rand(1)[0]
+    r = np.sqrt(1 / cent)
+    a = r * np.cos(theta)
+    b = r * np.sin(theta)
+    
+    return [a,b]
+
+locs = [drawer(node_color_ec[x]) for x in range(len(nations))]
+pos = dict(zip(nations, locs))
+
+labels = {}
+keeps_ids = [15, 29, 33, 57, 62, 79, 81, 101, 107, 125, 143, 162, 164]
+keeps = [nations[k] for k in keeps_ids]
+
+for n in N.nodes():
+    
+    if n in keeps:
+        
+        labels[n] = n
+
+#pos = nx.spring_layout(N, k = 150)
+#nodes_bc = nx.draw_networkx_nodes(N, pos, node_size = node_size, node_color = node_color_bc, alpha = 0.5)
+nodes_ec = nx.draw_networkx_nodes(N, pos, node_size = node_size, node_color = node_color_ec, alpha = 0.5)
+edges = nx.draw_networkx_edges(N, pos, edge_color = 'lightgray', arrows = False, width = 0.05)
+nx.draw_networkx_labels(N, pos, labels, font_size = 5)
+cbar = plt.colorbar(mappable = nodes_ec, cax = None, ax = None, fraction = 0.015, pad = 0.04)
+cbar.set_label('Eigenvector Centrality')
+plt.title('Global Timber Trade Network - 2017', fontsize = 12)
+plt.margins(.0666,.0666)
+plt.axis('off')
+plt.savefig(direc + 'figures/timber_trade_network.png', dpi = 1000)
+plt.savefig(direc + 'figures/timber_trade_network.eps', dpi = 1000)
+
+# Creating a sub-network of important nations
+
+N2 = nx.subgraph(N, keeps)
+
+keeps_ids2 = [29, 143, 107, 162, 62, 15, 81, 57, 79, 164, 33, 125, 101]
+keeps2 = [nations[k] for k in keeps_ids2]
+locs2 = [locs[x] for x in keeps_ids2]
+pos2 = dict(zip(keeps2, locs2))
+labels2 = {k:k for k in keeps2}
+node_size2 = [node_size[x] for x in keeps_ids2]
+node_color_ec2 = [node_color_ec[x] for x in keeps_ids2]
+nodes_ec2 = nx.draw_networkx_nodes(N2, pos2, node_size = node_size2, node_color = node_color_ec2, alpha = 0.5)
+edges2 = nx.draw_networkx_edges(N2, pos2, edge_color = 'gray', arrows = True, width = 0.05)
+nx.draw_networkx_labels(N2, pos2, labels2, font_size = 5)
+cbar = plt.colorbar(mappable = nodes_ec2, cax = None, ax = None, fraction = 0.015, pad = 0.04)
+cbar.set_label('Eigenvector Centrality')
+plt.title('Global Timber Trade Network - 2017: Highest Eigenvector Centralities', fontsize = 12)
+plt.margins(.0666,.0666)
+plt.axis('off')
+plt.savefig(direc + 'figures/timber_trade_network2.png', dpi = 1000)
+plt.savefig(direc + 'figures/timber_trade_network2.eps', dpi = 1000)
+
+# Repeating for the competition graph
+
+A = cg.competition_graph(M)
+G = nx.Graph(A)
+
+bcg = nx.betweenness_centrality(G, weight = 'weight')
+ecg = nx.eigenvector_centrality(G, weight = 'weight')
+nx.set_node_attributes(G, bcg, 'bc')
+nx.set_node_attributes(G, ecg, 'ec')
+nx.set_node_attributes(G, te, 'te')
+node_color_bcg = [float(G.nodes[v]['bc']) for v in G]
+node_color_ecg = [float(G.nodes[v]['ec']) for v in G]
+node_sizeg = [10*float(G.nodes[v]['te']) / mute for v in G]
+
+nodes_ecg = nx.draw_networkx_nodes(G, pos, node_size = node_sizeg, node_color = node_color_ecg, alpha = 0.5)
+edgesg = nx.draw_networkx_edges(G, pos, edge_color = 'lightgray', arrows = False, width = 0.05)
+nx.draw_networkx_labels(G, pos, labels, font_size = 5)
+cbar = plt.colorbar(mappable = nodes_ecg, cax = None, ax = None, fraction = 0.015, pad = 0.04)
+cbar.set_label('Eigenvector Centrality')
+plt.title('Global Timber Trade Competition Graph - 2017', fontsize = 12)
+plt.margins(.0666,.0666)
+plt.axis('off')
+plt.savefig(direc + 'figures/timber_trade_comp_graph.png', dpi = 1000)
+plt.savefig(direc + 'figures/timber_trade_comp_graph.eps', dpi = 1000)
+
+G2 = nx.subgraph(G, keeps)
+
+keeps_ids2 = [29, 143, 107, 162, 62, 15, 81, 57, 79, 164, 33, 125, 101]
+keeps2 = [nations[k] for k in keeps_ids2]
+locs2 = [locs[x] for x in keeps_ids2]
+pos2 = dict(zip(keeps2, locs2))
+labels2 = {k:k for k in keeps2}
+node_sizeg2 = [node_sizeg[x] for x in keeps_ids2]
+node_color_ecg2 = [node_color_ecg[x] for x in keeps_ids2]
+nodes_ecg2 = nx.draw_networkx_nodes(G2, pos2, node_size = node_sizeg2, node_color = node_color_ecg2, alpha = 0.5)
+edgesg2 = nx.draw_networkx_edges(G2, pos2, edge_color = 'gray', arrows = False, width = 0.05)
+nx.draw_networkx_labels(N2, pos2, labels2, font_size = 5)
+cbar = plt.colorbar(mappable = nodes_ecg2, cax = None, ax = None, fraction = 0.015, pad = 0.04)
+cbar.set_label('Eigenvector Centrality')
+plt.title('Global Timber Trade Competition Graph - 2017: Highest Eigenvector Centralities', fontsize = 12)
+plt.margins(.0666,.0666)
+plt.axis('off')
+plt.savefig(direc + 'figures/timber_trade_comp_graph2.png', dpi = 1000)
+plt.savefig(direc + 'figures/timber_trade_comp_graph2.eps', dpi = 1000)
+
